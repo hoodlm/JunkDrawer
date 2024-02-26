d56b3c738e2ebe24236b8b44daec5b5bae8769cd
Logan M Hood <hoodlma@gmail.com>
2024-02-25 20:37:56 -0500

    rust: A bad tree with an ugly imperative depth-first iterator implementation

---
+++ rust/tree-reference/src/lib.rs
@@ -0,0 +1,196 @@
+struct TreeNode<'a, T> {
+    elem: T,
+    children: Vec<&'a TreeNode<'a, T>>,
+}
+
+impl<'a, T> TreeNode<'a, T> {
+    fn new(elem: T) -> TreeNode<'a, T> {
+        TreeNode {
+            elem: elem,
+            children: Vec::new(),
+        }
+    }
+
+    fn add_child(&mut self, child: &'a TreeNode<'a, T>) {
+        self.children.push(&child);
+    }
+
+    fn dfs_iter(&self) -> DfsIter<T> {
+        println!("dfs_iter!");
+        println!("how many children? {}", self.children.len());
+        let sub_iters: Vec<DfsIter<T>> = self.children.iter().map(|child| {
+            child.dfs_iter()
+        }).collect();
+        println!("sub iters count: {}", sub_iters.len());
+        DfsIter {
+            head: Some(self),
+            active_iter: None,
+            sub_iters: sub_iters,
+        }
+    }
+}
+
+struct DfsIter<'a, T> {
+    head: Option<&'a TreeNode<'a, T>>,
+    active_iter: Option<Box<DfsIter<'a, T>>>,
+    sub_iters: Vec<DfsIter<'a, T>>,
+}
+
+impl<'a, T> Iterator for DfsIter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut next: Option<Self::Item> = None;
+        if self.active_iter.is_none() {
+            let maybe_next_child = self.sub_iters.pop();
+            if maybe_next_child.is_some() {
+                self.active_iter = Some(Box::new(maybe_next_child.unwrap()));
+            }
+        }
+
+        match self.active_iter.as_mut() {
+            None => {
+                next = match self.head {
+                    None => None,
+                    Some(node) => Some(&node.elem),
+                };
+                self.head = None;
+            },
+            Some(active_iter) => {
+                next = active_iter.next();
+                if active_iter.head.is_none() {
+                    self.active_iter = None;
+                }
+            }
+        };
+        next
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty_tree() {
+        let tree = TreeNode::new(5);
+        
+        assert_eq!(tree.elem, 5);
+        assert_eq!(tree.children.len(), 0);
+    }
+
+    #[test]
+    fn tree_with_children() {
+        let mut tree = TreeNode::new(5);
+        let left = TreeNode::new(6);
+        let right = TreeNode::new(7);
+
+        tree.add_child(&left);
+        tree.add_child(&right);
+
+        assert_eq!(tree.children.len(), 2);
+        let values: Vec<i32> = tree.children.iter().map(|node| {
+            node.elem
+        }).collect();
+        assert_eq!(values, vec![6, 7])
+    }
+
+    #[test]
+    fn add_subtrees() {
+        let mut head = TreeNode::new(String::from("head"));
+
+        let mut left_subtree = TreeNode::new(String::from("head/left"));
+        let left_leaf_1 = TreeNode::new(String::from("head/left/leaf1"));
+        let left_leaf_2 = TreeNode::new(String::from("head/left/leaf2"));
+        left_subtree.add_child(&left_leaf_1);
+        left_subtree.add_child(&left_leaf_2);
+
+        let mut right_subtree = TreeNode::new(String::from("head/left"));
+        let right_leaf_1 = TreeNode::new(String::from("head/right/leaf1"));
+        let right_leaf_2 = TreeNode::new(String::from("head/right/leaf2"));
+        right_subtree.add_child(&right_leaf_1);
+        right_subtree.add_child(&right_leaf_2);
+
+        head.add_child(&left_subtree);
+        head.add_child(&right_subtree);
+
+        assert_eq!(head.children.len(), 2);
+
+        let left = head.children.pop().unwrap();
+        assert_eq!(left.elem, "head/left");
+        assert_eq!(left.children.len(), 2);
+
+        let right = head.children.pop().unwrap();
+        assert_eq!(right.elem, "head/left");
+        assert_eq!(right.children.len(), 2);
+    }
+
+    #[test]
+    fn dfs_two_levels() {
+        let mut head = TreeNode::new(String::from("head"));
+
+        let mut left_subtree = TreeNode::new(String::from("head/left"));
+        let left_leaf_1 = TreeNode::new(String::from("head/left/leaf1"));
+        let left_leaf_2 = TreeNode::new(String::from("head/left/leaf2"));
+        let left_leaf_3 = TreeNode::new(String::from("head/left/leaf3"));
+        left_subtree.add_child(&left_leaf_1);
+        left_subtree.add_child(&left_leaf_2);
+        left_subtree.add_child(&left_leaf_3);
+
+        let mut right_subtree = TreeNode::new(String::from("head/right"));
+        let right_leaf_1 = TreeNode::new(String::from("head/right/leaf1"));
+        let right_leaf_2 = TreeNode::new(String::from("head/right/leaf2"));
+        right_subtree.add_child(&right_leaf_1);
+        right_subtree.add_child(&right_leaf_2);
+
+        head.add_child(&left_subtree);
+        head.add_child(&right_subtree);
+
+        let mut iter = head.dfs_iter();
+        assert_eq!(iter.next(), Some(&String::from("head/right/leaf2")));
+        assert_eq!(iter.next(), Some(&String::from("head/right/leaf1")));
+        assert_eq!(iter.next(), Some(&String::from("head/right")));
+        assert_eq!(iter.next(), Some(&String::from("head/left/leaf3")));
+        assert_eq!(iter.next(), Some(&String::from("head/left/leaf2")));
+        assert_eq!(iter.next(), Some(&String::from("head/left/leaf1")));
+        assert_eq!(iter.next(), Some(&String::from("head/left")));
+        assert_eq!(iter.next(), Some(&String::from("head")));
+        assert_eq!(iter.next(), None);
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn dfs_deep_levels() {
+        let mut head = TreeNode::new(String::from("head"));
+        let mut level_1 = TreeNode::new(String::from("head/1"));
+        let mut level_2 = TreeNode::new(String::from("head/1/2"));
+        let mut level_3 = TreeNode::new(String::from("head/1/2/3"));
+        let mut level_4 = TreeNode::new(String::from("head/1/2/3/4"));
+        let mut level_5 = TreeNode::new(String::from("head/1/2/3/4/5"));
+        let mut level_6 = TreeNode::new(String::from("head/1/2/3/4/5/6"));
+        let level_6a = TreeNode::new(String::from("head/1/2/3/4/5/6/A"));
+        let level_6b = TreeNode::new(String::from("head/1/2/3/4/5/6/B"));
+
+        level_6.add_child(&level_6a);
+        level_6.add_child(&level_6b);
+
+        level_5.add_child(&level_6);
+        level_4.add_child(&level_5);
+        level_3.add_child(&level_4);
+        level_2.add_child(&level_3);
+        level_1.add_child(&level_2);
+        head.add_child(&level_1);
+
+        let mut dfs = head.dfs_iter();
+        assert_eq!(dfs.next(), Some(&String::from("head/1/2/3/4/5/6/B")));
+        assert_eq!(dfs.next(), Some(&String::from("head/1/2/3/4/5/6/A")));
+        assert_eq!(dfs.next(), Some(&String::from("head/1/2/3/4/5/6")));
+        assert_eq!(dfs.next(), Some(&String::from("head/1/2/3/4/5")));
+        assert_eq!(dfs.next(), Some(&String::from("head/1/2/3/4")));
+        assert_eq!(dfs.next(), Some(&String::from("head/1/2/3")));
+        assert_eq!(dfs.next(), Some(&String::from("head/1/2")));
+        assert_eq!(dfs.next(), Some(&String::from("head/1")));
+        assert_eq!(dfs.next(), Some(&String::from("head")));
+        assert_eq!(dfs.next(), None);
+        assert_eq!(dfs.next(), None);
+    }
+}
