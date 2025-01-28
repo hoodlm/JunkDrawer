7fb037c8f7e9670f90e3fd8c18bf32dcf10a8acd
Logan M Hood <hoodlm@protonmail.com>
2025-01-28 13:28:33 -0500

    tl-toolkit: make byteutil less panicky

--- rust/tl-toolkit/src/byteutil.rs
+++ rust/tl-toolkit/src/byteutil.rs
@@ -2,7 +2,14 @@ use std::ops::Range;
 
 pub fn u16_at_addr(buffer: &Vec<u8>, addr: usize) -> Result<u16, String> {
     let range: Range<usize> = addr..addr + 2;
-    let bytes: [u8; 2] = buffer.get(range).unwrap().try_into().unwrap();
+    let bytes: [u8; 2] = buffer
+        .get(range)
+        .ok_or(format!(
+            "There are not 2 bytes from address {addr} - buffer is only {0} bytes",
+            buffer.len()
+        ))?
+        .try_into()
+        .unwrap();
     let result: u16 = u16::from_le_bytes(bytes)
         .try_into()
         .map_err(|e| format!("Two bytes at addr 0x{addr:x} are not a valid u16: {e}"))?;
@@ -11,7 +18,14 @@ pub fn u16_at_addr(buffer: &Vec<u8>, addr: usize) -> Result<u16, String> {
 
 pub fn u32_at_addr(buffer: &Vec<u8>, addr: usize) -> Result<u32, String> {
     let range: Range<usize> = addr..addr + 4;
-    let bytes: [u8; 4] = buffer.get(range).unwrap().try_into().unwrap();
+    let bytes: [u8; 4] = buffer
+        .get(range)
+        .ok_or(format!(
+            "There are not 4 bytes from address {addr} - buffer is only {0} bytes",
+            buffer.len()
+        ))?
+        .try_into()
+        .unwrap();
     let result: u32 = u32::from_le_bytes(bytes)
         .try_into()
         .map_err(|e| format!("Two bytes at addr 0x{addr:x} are not a valid u32: {e}"))?;
@@ -21,7 +35,13 @@ pub fn u32_at_addr(buffer: &Vec<u8>, addr: usize) -> Result<u32, String> {
 pub fn string_at_addr(buffer: &Vec<u8>, addr: usize, len: usize) -> Result<String, String> {
     let end: usize = addr + len;
     let range: Range<usize> = addr..end;
-    let bytes: Vec<u8> = buffer.get(range).unwrap().to_vec();
+    let bytes: Vec<u8> = buffer
+        .get(range)
+        .ok_or(format!(
+            "There are not {len} bytes from address {addr} - buffer is only {0} bytes",
+            buffer.len()
+        ))?
+        .to_vec();
     let s = String::from_utf8(bytes)
         .map_err(|e| format!("Bytes in range 0x{addr:x}-0x{end:x} are not valid utf8: {e}"))?;
     Ok(s)
