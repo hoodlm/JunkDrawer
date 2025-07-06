b951e02428692230f5c9007b1a6cdb21717ddf2b
Logan M Hood <hoodlm@protonmail.com>
2025-07-06 15:34:38 -0400

    Testbed for gemma3n

--- bots/ollama-hello-world/hello.rb
+++ bots/ollama-hello-world/hello.rb
@@ -34,8 +34,65 @@ class HelloOllama
 end
 
 bot = HelloOllama.new({
-  model: "qwen3:14b",
-  think: true,
+  model: "gemma3n:latest",
+  think: false,
 })
 
-bot.one_shot("Tell me a joke about computers")
+def build_prompt(expense)
+<<-PROMPT
+Pick the best category for the given expense. Respond with a brief justification, the name of the category from the list below, and confidence 1-5.
+
+EXAMPLE INPUT 1:
+Expense: "Zoo"
+
+EXAMPLE OUTPUT 2:
+justification: "The Zoo is a recreational activity one would do with a family"
+category: "Entertainment"
+confidence: 5
+
+
+EXAMPLE INPUT 1:
+Expense: "7-11"
+
+EXAMPLE OUTPUT 2:
+justification: "7-11 probably refers to the convenience store. One could purchase gas or convenience store items there, but more likely gas"
+category: "Transportation"
+confidence: 2
+
+Choose from only these categories:
+
+<categories>
+Food
+Entertainment
+Household Items
+Utilities
+Home Improvement
+Transportation
+</categories>
+
+---
+
+Expense: "#{expense}"
+PROMPT
+end
+
+[
+  "Human Service Department",
+  "Indian Mountain Machine Shop",
+  "UBank",
+  "Jellico Electric & Water System",
+  "Subway",
+  "Days Inn",
+  "McDonald's",
+  "Shell",
+  "Douglas Oil Shop",
+  "Crouches Creek Baptist Church",
+  "Patriotic Palace",
+  "Jimmys Market 18",
+  "Rolling Coal",
+  "bp",
+  "Douglas Cemetery #4",
+].each do |expense|
+  prompt = build_prompt(expense)
+  bot.one_shot(prompt)
+end
